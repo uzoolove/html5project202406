1cce429d42d52e383f2f6c6cacedca8ea67ec191
uzoolove <uzoolove@gmail.com>
2024-06-11 09:36:36 +0900

    .

--- workspace-ins/mulpang/doc/01_memberdbtest_for_shell.js
+++ workspace-ins/mulpang/doc/01_memberdbtest_for_shell.js
@@ -18,33 +18,41 @@ db.member.find();
 
 // TODO 3. 회원 조회(나이가 20인 회원 조회)
 // find({검색조건})
+db.member.find({ age: 20 });
 
 // name이 admin
+db.member.find({ name: 'admin' });
 
 // name이 lee이고 age가 20
+db.member.find({ name: 'lee', age: 20 });
 
 // age가 35 이상
-
-
+db.member.find({ age: { $gte: 35 } });
 
 // TODO 4. 회원 조회(1건)
 // findOne()
-
+db.member.findOne();
+db.member.findOne({ name: 'lee' });
+db.member.findOne({ age: 20, name: 'kim' });
+db.member.findOne({ _id: 123 });
+db.member.findOne({ age: 20 });
 
 // TODO 5. 회원 수정(kim의 나이 수정)
 // 지정한 문서를 교체
 // replaceOne({검색조건}, {수정할 문서})
+db.member.replaceOne({ name: 'kim' }, { age: 21 });
 
 // 지정한 속성만 수정할 경우
 // updateOne({검색조건}, {$set: {수정할 속성}})
+db.member.updateOne({ name: 'lee'}, { $set: { age: 21 } });
 
 // 지정한 속성을 증가시킬 경우
 // updateOne({검색조건}, {$inc: {증가할 속성}})
-
+db.member.updateOne({ name: 'lee'}, { $inc: { age: 1 } });
 
 // TODO 6. lee 삭제
 // deleteOne({검색 조건})
-
+db.member.deleteOne({ name: 'lee' });
 
 
 
